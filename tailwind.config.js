ab24c9f6d52b6d63e88c121aaf03242671e7e8ae
Abdallah Bari <abdellah01win@gmail.com>
2023-05-23 22:51:05 +0100

    add initial color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'myYellow': '#D5FF40',
+        'myBlue': {
+          '100' : '#3A53E3',
+        },
+        'myDark': {
+          '100': '#101010',
+          '200': '#171717',
+          '300': '#29282B'
+        },
+        'myGray': {
+          '100': '#F8F8F8',
+          '200': '#C4C4C4',
+        },
+      }
+    },
   },
   plugins: [],
 }
