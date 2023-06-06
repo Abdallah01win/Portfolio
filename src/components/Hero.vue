b7938625e9580814d666115dbd0bdb33ad773b4e
Abdallah Bari <abdellah01win@gmail.com>
2023-06-06 01:18:18 +0100

    redo hero circle and restyle social links

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -20,9 +20,9 @@ export default {
         onMounted(() => {
             if (circleTextRef.value) {
                 circleTextRef.value.innerHTML = circleTextRef.value.innerText
-                    .split('')
-                    .map((char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`)
-                    .join('');
+                    .split("")
+                    .map((char, i) => `<span style="transform:rotate(${i * 8.3}deg); transform-origin:0px 100px;" class="absolute left-[50%]">${char}</span>`)
+                    .join("");
             }
         });
         return {
@@ -57,41 +57,40 @@ export default {
                 </button>
             </div>
         </div>
-        <div class="flex gap-x-8 w-fit absolute bottom-[50%] right-0 /pb-24 translate-y-[50%]">
-            <div class="flex items-center justify-center w-[200px] h-[200px] rounded-full border border-myDark-100 relative">
+        <div class="flex gap-x-8 w-fit absolute bottom-0 right-0 pb-24 /translate-y-[-50%]">
+            <!-- <div class="flex items-center justify-center w-[200px] h-[200px] rounded-full border border-myDark-100 relative">
                 <div class="w-full h-full absolute">
                     <div ref="circleTextRef" class="text">Abdallah Bari - Web Development</div>
                 </div>
-                <!-- <ArrowUp class="w-8" /> -->
-            </div>
-            <div></div>
-            <div class="flex flex-col justify-evenly">
-                <span>
+                <ArrowUp class="w-8" />
+            </div> -->
+            <div class="flex flex-col items-center /justify-evenly gap-y-4">
+                <!-- <div class="/h-14 border-r border-myDark-100 mb-1"></div> -->
+                <div>
                     <a href="https://github.com/Abdallah01win" target="_blank">
                         <GithubLogo class="w-6 fill-myDark-100" />
                     </a>
-                </span>
-                <span>
+                </div>
+                <div>
                     <a href="https://twitter.com/AbdallahDev01" target="_blank">
                         <TwitterLogo class="w-6 fill-myDark-100" />
                     </a>
-                </span>
-                <span>
+                </div>
+                <div>
                     <a href="https://www.linkedin.com/in/abdallahbari/" target="_blank">
                         <LinkedInLogo class="w-6 fill-myDark-100" />
                     </a>
-                </span>
+                </div>
+
+                <div class="h-16 border-r border-myDark-100 mt-1"></div>
             </div>
         </div>
     </main>
 </template>
 
 <style scoped>
+
 .text span{
-    position: absolute;
-    left: 50%;
-    font: 1.2em;
-    color: brown;
     transform-origin: 0 100px;
 }
 .hero {
@@ -105,5 +104,4 @@ export default {
         radial-gradient(at 0% 65%, hsla(253, 64%, 69%, 1) 0px, transparent 50%),
         radial-gradient(at 77% 74%, hsla(264, 78%, 78%, 1) 0px, transparent 50%);
 }
-
 </style>
