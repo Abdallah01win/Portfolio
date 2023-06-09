962187754e659d191b88769069eb9515b02724cb
Abdallah Bari <abdellah01win@gmail.com>
2023-06-09 13:46:20 +0100

    icon switch in navigation and initial restyle in contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,61 +1,80 @@
 <script setup>
-import ArrowUp from './icons/ArrowUp.vue'
+import ArrowUp from "./icons/ArrowUp.vue";
 </script>
 <template>
-    <section class="xl:max-w-[1024px] mx-auto my-12">
-        <!-- <h2 class="text-3xl font-bold mb-6">
+  <section class="xl:max-w-[1024px] mx-auto my-12">
+    <!-- <h2 class="text-3xl font-bold mb-6">
             <span>Contact </span>
             <span class="text-myGray-500 italic">Me</span>
         </h2> -->
-        <div class="bg-myGray-600 p-10 rounded-3xl grid grid-cols-2 gap-x-8">
-            <div>
-                <!-- <div>
-                    <h2 class="text-3xl font-bold mb-4">
-                        <span>Contact </span>
-                        <span class="text-myGray-500 italic">Me</span>
-                    </h2>
-                </div> -->
-                <h2 class="text-3xl font-bold mb-1">Have an Idea? I got the skills.</h2>
-                <p class="leading-6">Let's work together to make your visions a reality. Tell us more about yourself
-                    and
-                    what's on your mind.</p>
-            
-            <form action="#" method="post" class="flex flex-col gap-y-2 mt-4">
-                <input type="text" name="name" id="name" placeholder="Your name"
-                    class="bg-transparent border-b border-myDark-100 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none">
-                <input type="email" name="email" id="email" placeholder="Email Adress"
-                    class="bg-transparent border-b border-myDark-100 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none">
-                <select name="intrest" id="intrest"
-                    class="bg-transparent border-b border-myDark-100 py-2 focus:outline-none select">
-                    <option value="full-stack">Full-Stack Web Application</option>
-                    <option value="front">Front-End Web Application</option>
-                    <option value="api">API Development</option>
-                    <option value="other">Other</option>
-                </select>
-                <input type="text" name="description" placeholder="Short description of your project"
-                    class="bg-transparent border-b border-myDark-100 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none">
-                <button
-                    class="rounded-full py-2 px-6 bg-myDark-100 text-myWhite gap-x-2 flex justify-center font-normal mt-5">
-                    <span>Send Email</span>
-                    <span>
-                        <ArrowUp class="w-5" />
-                    </span>
-                </button>
-            </form>
+    <div class="bg-myGray-600 p-10 rounded-3xl">
+      <div class="mb-4">
+        <h2 class="text-5xl font-bold mb-3">Have an Idea? I got the skills.</h2>
+        <p class="leading-6">
+          Let's work together to make your visions a reality. Tell us more about
+          yourself and what's on your mind. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium quibusdam cupiditate doloribus, architecto voluptates optio mollitia obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div class="grid grid-cols-2 gap-x-8">
+        <form action="#" method="post" class="flex flex-col gap-y-2 mt-4">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+            class="bg-transparent border-b border-myGray-500 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Adress"
+            class="bg-transparent border-b border-myGray-500 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none"
+          />
+          <select
+            name="intrest"
+            id="intrest"
+            class="bg-transparent border-b border-myGray-500 py-2 focus:outline-none select"
+          >
+            <option value="full-stack">Full-Stack Web Application</option>
+            <option value="front">Front-End Web Application</option>
+            <option value="api">API Development</option>
+            <option value="other">Other</option>
+          </select>
+          <input
+            type="text"
+            name="description"
+            placeholder="Short description of your project"
+            class="bg-transparent border-b border-myGray-500 py-2 placeholder:text-myGray-500 placeholder:font-normal focus:outline-none"
+          />
+          <button
+            class="rounded-full py-3 px-6 bg-myDark-100 text-myWhite gap-x-2 flex justify-center font-normal mt-5"
+          >
+            <span>Send Email</span>
+            <span>
+              <ArrowUp class="w-5" />
+            </span>
+          </button>
+        </form>
+        <div class="flex items-center justify-center ">
+            <div class="border border-myDark-100 rounded-full p-20">
+                <ArrowUp class="w-8"/>
+            </div>
         </div>
-        </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
 .select {
-    color: #969696;
-    appearance: none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='%23000000' viewBox='0 0 256 256'><path d='M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z'></path></svg>");
-    background-repeat: no-repeat;
-    background-position-x: 98%;
-    background-position-y: 50%;
+  color: #969696;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='%23969696' viewBox='0 0 256 256'><path d='M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z'></path></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 98%;
+  background-position-y: 50%;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,12 +2,14 @@
 import ArrowUp from "./icons/ArrowUp.vue";
 import Glob from "./icons/Glob.vue";
 import List from "./icons/List.vue";
+import X from "./icons/X.vue"
 import { ref, onMounted } from "vue";
 export default {
   components: {
     ArrowUp,
     Glob,
     List,
+    X,
   },
   setup() {
     const isNavOpen = ref(false);
@@ -18,28 +20,32 @@ export default {
       isNavOpen,
       toggelMenu,
     };
-  }
-}
+  },
+};
 </script>
 <template>
   <nav class="flex items-center justify-between py-4 xl:max-w-[1024px] mx-auto">
     <div class="font-bold text-lg">Abdallah</div>
 
     <div class="flex items-center /gap-x-4">
-      <button class="bg-myDark-100 text-myWhite rounded-full py-2 px-8 mr-4 flex items-center gap-x-1 font-normal">
+      <button
+        class="bg-myDark-100 text-myWhite rounded-full py-2 px-8 mr-4 flex items-center gap-x-1 font-normal"
+      >
         <span>Get in-touch</span>
         <span>
           <ArrowUp class="w-5" />
         </span>
       </button>
 
-      <div class="flex items-center gap-x-2 border border-myDark-100 rounded-full py-2 px-3 cursor-pointer font-normal">
+      <div
+        class="flex items-center gap-x-2 border border-myDark-100 rounded-full py-2 px-3 cursor-pointer font-normal"
+      >
         <Glob class="w-5" />
         <span>En</span>
       </div>
 
-      <hr class="hr /rotate-90 h-8 w-[1px] bg-myGray-500 mx-4">
-      <div :class="['navigation', { 'show': isNavOpen }]">
+      <hr class="hr /rotate-90 h-8 w-[1px] bg-myGray-500 mx-4" />
+      <div :class="['navigation', { show: isNavOpen }]">
         <ul class="flex items-center gap-x-8 nav-links">
           <li>About</li>
           <li>Projects</li>
@@ -47,9 +53,12 @@ export default {
         </ul>
       </div>
 
-      <div class="grid place-content-center p-2 border border-myDark-100 rounded-full cursor-pointer toggle-button"
-        @click="toggelMenu()">
-        <List class="w-5" />
+      <div
+        class="grid place-content-center p-2 border border-myDark-100 rounded-full cursor-pointer toggle-button"
+        @click="toggelMenu()"
+      >
+      <X v-if="isNavOpen" class="w-5" />
+      <List v-else class="w-5" />
       </div>
     </div>
   </nav>
@@ -73,5 +82,25 @@ export default {
 
 .toggle-button {
   cursor: pointer;
+}
+
+@keyframes x-animation {
+  0% {
+    transform: rotate(0);
+  }
+  50% {
+    transform: rotate(25deg);
+  }
+  100% {
+    transform: rotate(45deg);
+  }
+}
+
+.svg-container path {
+  animation-name: x-animation;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
 }
 </style>
