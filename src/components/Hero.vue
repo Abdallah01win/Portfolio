5b6349ae43098aaca077634039f42003abe57120
Abdallah Bari <abdellah01win@gmail.com>
2023-09-09 01:35:29 +0100

    Initial footer layout build

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,5 @@
 <script setup>
 import Navigation from "@/components/Navigation.vue";
-import GithubLogo from "@/components/icons/GithubLogo.vue";
-import LinkedInLogo from "@/components/icons/LinkedInLogo.vue";
-import TwitterLogo from "@/components/icons/TwitterLogo.vue";
 
 setInterval(function () {
     const show = document.querySelector('.mask span[data-show]')
@@ -47,7 +44,7 @@ setInterval(function () {
 </template>
 
 
-<style>
+<style scoped>
 .mask {
     height: 186px;
     position: relative;
@@ -79,19 +76,8 @@ setInterval(function () {
 }
 
 .mesh {
-    background-image: url('../assets/images/Texture.png'),
+    background-image: url('@/assets/images/Texture.png'),
         radial-gradient(at 41% 145%, rgb(97, 34, 214) 0, hsla(240, 85%, 69%, 0) 50%),
         radial-gradient(at 130% 110%, rgb(59, 104, 230) 0, hsla(192, 91%, 57%, 0) 50%);
-
-
-
-
-    /* background-image: radial-gradient(at 21% 59%, hsla(225, 88%, 50%, 1) 0, hsla(225, 88%, 50%, 0) 50%),
-        radial-gradient(at 2% 68%, hsla(249, 95%, 63%, 1) 0, hsla(249, 95%, 63%, 0) 50%),
-        radial-gradient(at 66% 35%, hsla(219, 90%, 51%, 1) 0, hsla(219, 90%, 51%, 0) 50%); */
-
-    /* background-image: radial-gradient(at 9% 48%, hsla(259, 88%, 58%, 1) 0, hsla(259, 88%, 58%, 0) 50%),
-        radial-gradient(at 28% 66%, hsla(238, 95%, 65%, 1) 0, hsla(238, 95%, 65%, 0) 50%),
-        radial-gradient(at 35% 0%, hsla(220, 92%, 52%, 1) 0, hsla(220, 92%, 52%, 0) 50%); */
 }
 </style>
