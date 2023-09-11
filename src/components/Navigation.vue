7fcfcfea100c66581aec349755c4d2ab8c3754c6
Abdallah Bari <abdellah01win@gmail.com>
2023-09-11 23:15:02 +0100

    Add navigation links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,26 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 <template>
   <section class="text-white">
     <nav
       class="flex items-center justify-between py-5 border-b border-myGray-400/30 xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto">
       <RouterLink to="/" class="font-bold text-xl">Abdallah<span class="text-myGray-500 italic">Bari</span></RouterLink>
+
+      <ul class="flex items-center gap-x-8 tracking-wide text-sm text-myGray-500">
+        <li class="hover:text-white cursor-pointer transition-all">
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li class="hover:text-white cursor-pointer transition-all">
+          <RouterLink to="/">About</RouterLink>
+        </li>
+        <li class="hover:text-white cursor-pointer transition-all">
+          <RouterLink to="/">Services</RouterLink>
+        </li>
+        <li class="hover:text-white cursor-pointer transition-all">
+          <RouterLink to="/">Projects</RouterLink>
+        </li>
+      </ul>
 
       <div class="cursor-pointer border border-myGray-400/40 rounded-full px-8 py-2 tracking-wide text-sm">Contact</div>
     </nav>
