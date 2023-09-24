35e3c2562c4554df6bc32c7d54b093043a72c44c
Abdallah Bari <abdellah01win@gmail.com>
2023-09-24 18:18:21 +0100

    Write about me section copy

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,24 +3,18 @@ import { RouterLink } from "vue-router";
 import ArrowUp from "@/components/icons/ArrowUp.vue";
 </script>
 <template>
-  <section
-    class="xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto py-24 border-b border-myGray-500/40">
+  <section class="xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto py-24 border-b border-myGray-500/40">
     <div class="grid grid-cols-[.6fr,1fr]">
-      <div
-        class="border border-myDark-100 py-2 px-6 w-fit h-fit rounded-full text-lg font-semibold mb-10 capitalize">
+      <div class="border border-myDark-100 py-2 px-6 w-fit h-fit rounded-full text-lg font-semibold mb-10 capitalize">
         about me
       </div>
       <div>
-        <p class="font-primary text-5xl font-medium leading-[56px] mb-8">
-          We have put together a special combination of strategy, marketting,
-          design and digital experties.
+        <p class="font-primary text-5xl font-semibold leading-[1.1] mb-8">
+          I turn websites into powerful online assets that maximize your digital potential.
         </p>
         <div>
           <div class="leading-8 text-lg font-medium">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            soluta corrupti non labore corporis dolorum. Lorem ipsum dolor sit
-            amet consectetur, adipisicing elit. Perspiciatis, debitis? Lorem
-            ipsum dolor sit amet.
+            My name is Abdallah Bari, I'm a seasoned web developer dedicated to crafting exceptional online experiences. With a rich background in web development spanning over 4 years, i've picked up a special combination of strategy, design, and marketting that enables me to deliver, not only, flawlessly functioning, but also esthetically pleasing websites with a lasting impression.
           </div>
         </div>
 
