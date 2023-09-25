cd121d4f33feeb603346809c6233eaf2c0b0b83d
Abdallah Bari <abdellah01win@gmail.com>
2023-09-25 23:12:29 +0100

    Initial experiance section copy

--- src/components/ExperianceSection.vue
+++ src/components/ExperianceSection.vue
@@ -5,12 +5,14 @@ import ArrowUp from "@/components/icons/ArrowUp.vue";
 <template>
     <section class="bg-myDark-100">
         <div class="xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto py-24 text-white">
-            <div class="border border-white py-2 px-6 w-fit rounded-full text-lg font-medium mb-10 capitalize">why choose me?
+            <div class="border border-white py-2 px-6 w-fit rounded-full text-lg font-medium mb-10 capitalize">why choose
+                me?
             </div>
-            <div class="grid grid-cols-[1fr,.7fr]">
+            <div class="grid grid-cols-[1fr,1fr]">
                 <div>
-                    <p class="w-[90%] font-primary text-5xl leading-[56px]">I deliver the best results for my clients in the
-                        shortest time.</p>
+                    <p class="w-[90%] font-primary text-5xl leading-[56px]">
+                        I'm not here to push pre-made solutions onto your project.
+                    </p>
 
                     <button
                         class="bg-white text-myDark-100 text-lg mt-10 tracking-wide font-semibold rounded-full py-2 px-8 mr-4 flex items-center gap-x-4 ">
@@ -22,22 +24,22 @@ import ArrowUp from "@/components/icons/ArrowUp.vue";
                 </div>
 
                 <div>
-                    <p class="leading-8 text-lg font-medium mb-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Molestias porro earum qui rerum, distinctio
-                        dignissimos quisquam. Nihil explicabo iusto, eligendi earum assumenda nisi atque? Nostrum, voluptas!
-                        Fuga ut
-                        tempore, suscipit omnis non in debitis fugiat nemo atque hic.</p>
+                    <p class="leading-8 text-lg font-medium mb-8">
+                        I'm here to listen, understand your unique needs, and tailor a web solution that's as unique as your
+                        business. Expect open and transparent communication throughout our partnership, ensuring that your
+                        ideas are not just heard but also brilliantly executed.
+                    </p>
                     <p class="leading-8 text-lg font-medium">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                         Molestias
                         porro earum qui rerum, distinctio
-                        dignissimos quisquam. Nihil explicabo iusto, eligendi earum assumenda nisi atque? Nostrum, voluptas!
-                        Fuga.
+                        dignissimos quisquam. Nihil explicabo iusto. Your success is my success, and I'm
+                        dedicated to make it happen.
                     </p>
                 </div>
             </div>
             <div class="flex items-center justify-between mt-16">
                 <div>
-                    <div class="text-7xl mb-3 font-medium">03+</div>
+                    <div class="text-7xl mb-3 font-medium">04+</div>
                     <div class="text-xl">Years of Experiance</div>
                 </div>
                 <div>
@@ -50,5 +52,4 @@ import ArrowUp from "@/components/icons/ArrowUp.vue";
                 </div>
             </div>
         </div>
-    </section>
-</template>
+</section></template>
