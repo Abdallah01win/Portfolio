0d5c466b40010261d1393d136e1e2cccae14787b
Abdallah Bari <abdellah01win@gmail.com>
2023-10-27 23:35:25 +0100

    Add projects images in webp

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -11,15 +11,27 @@ const projects = [
   },
   {
     title: "doluma",
-    images: [`${path}doluma/1.webp`],
+    images: [
+      `${path}doluma/1.webp`,
+      `${path}doluma/2.webp`,
+      `${path}doluma/3.webp`,
+    ],
   },
   {
     title: "zaniture",
-    images: [`${path}zaniture/1.webp`],
+    images: [
+      `${path}zaniture/1.webp`,
+      `${path}zaniture/2.webp`,
+      `${path}zaniture/3.webp`,
+    ],
   },
   {
-    title: "buildinz",
-    images: [`${path}buildinz/1.webp`],
+    title: "wondscape",
+    images: [
+      `${path}wondscape/1.png`,
+      `${path}wondscape/2.png`,
+      `${path}wondscape/3.png`,
+    ],
   },
 ];
 
