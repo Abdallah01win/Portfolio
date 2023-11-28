d650fd2d9a2b36a2e72c3b5f99f12208cc18d857
Abdallah Bari <abdellah01win@gmail.com>
2023-11-28 21:54:02 +0100

    move testemonials to js module and run prettier formatting

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -1,67 +1,82 @@
 <script setup>
 import ArrowUp from "@/components/icons/ArrowUp.vue";
 
-const address = import.meta.env.VITE_MAIL_TO_ADDRESS
+const address = import.meta.env.VITE_MAIL_TO_ADDRESS;
 </script>
 
 <template>
-    <section class="bg-myDark-100 texture">
-        <div
-            class="3xs:max-w-[320px] 2xs:max-w-[360px] xs:max-w-[390px] sm:max-w-[475px] md:max-w-[640px] lg:max-w-[768px] xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto py-14 sm:py-18 md:py-24 text-white">
-            <div
-                class="border border-white py-2 px-6 w-fit rounded-full text-base lg:text-lg font-medium mb-6 sm:mb-8 lg:mb-10 capitalize">
-                why choose me?
-            </div>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-0">
-                <div>
-                    <p class="lg:w-[90%] font-primary text-3xl xs:text-4xl xl:text-5xl leading-[1.35] md:leading-[1.1]">
-                        I'm not here to push pre-made solutions onto your project.
-                    </p>
+  <section class="bg-myDark-100 texture">
+    <div
+      class="3xs:max-w-[320px] 2xs:max-w-[360px] xs:max-w-[390px] sm:max-w-[475px] md:max-w-[640px] lg:max-w-[768px] xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto py-14 sm:py-18 md:py-24 text-white"
+    >
+      <div
+        class="border border-white py-2 px-6 w-fit rounded-full text-base lg:text-lg font-medium mb-6 sm:mb-8 lg:mb-10 capitalize"
+      >
+        why choose me?
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-0">
+        <div>
+          <p
+            class="lg:w-[90%] font-primary text-3xl xs:text-4xl xl:text-5xl leading-[1.35] md:leading-[1.1]"
+          >
+            I'm not here to push pre-made solutions onto your project.
+          </p>
 
-                    <a :href="`https://mail.google.com/mail/?view=cm&fs=1&to=${address}`" target="_blank"
-                        class="bg-white text-myDark-100 text-lg mt-10 tracking-wide font-semibold rounded-full py-2 px-8 mr-4 flex items-center gap-x-4 w-fit">
-                        <span>Let's work together!</span>
-                        <span>
-                            <ArrowUp class="w-6" />
-                        </span>
-                    </a>
-                </div>
-
-                <div class="leading-8 text-base sm:text-lg font-medium">
-                    <p class="mb-8">
-                        I'm here to listen, understand your unique needs, and tailor a web solution that's as unique as your
-                        business. Expect open and transparent communication throughout our partnership, ensuring that your
-                        ideas are not just heard but also brilliantly executed.
-                    </p>
-                    <p>
-                        Because excellence lies in the details, I pay carful attention
-                        to all aspects of a successful business website that will set you ahead of the competition. Your
-                        success is my success, I and I'm
-                        dedicated to make it happen.
-                    </p>
-                </div>
-            </div>
-            <div
-                class="flex items-center justify-between mt-16 text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-sm:text-center">
-                <div>
-                    <div class="mb-3 font-medium">05+</div>
-                    <div class="text-sm sm:text-base md:text-lg lg:text-xl">Years of Experience</div>
-                </div>
-                <div>
-                    <div class="mb-3 font-medium">25+</div>
-                    <div class="text-sm sm:text-base md:text-lg lg:text-xl">Satisfied Clients</div>
-                </div>
-                <div>
-                    <div class="mb-3 font-medium">40+</div>
-                    <div class="text-sm sm:text-base md:text-lg lg:text-xl">Completed Projects</div>
-                </div>
-            </div>
+          <a
+            :href="`https://mail.google.com/mail/?view=cm&fs=1&to=${address}`"
+            target="_blank"
+            class="bg-white text-myDark-100 text-lg mt-10 tracking-wide font-semibold rounded-full py-2 px-8 mr-4 flex items-center gap-x-4 w-fit"
+          >
+            <span>Let's work together!</span>
+            <span>
+              <ArrowUp class="w-6" />
+            </span>
+          </a>
         </div>
-    </section>
+
+        <div class="leading-8 text-base sm:text-lg font-medium">
+          <p class="mb-8">
+            I'm here to listen, understand your unique needs, and tailor a web
+            solution that's as unique as your business. Expect open and
+            transparent communication throughout our partnership, ensuring that
+            your ideas are not just heard but also brilliantly executed.
+          </p>
+          <p>
+            Because excellence lies in the details, I pay carful attention to
+            all aspects of a successful business website that will set you ahead
+            of the competition. Your success is my success, I and I'm dedicated
+            to make it happen.
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex items-center justify-between mt-16 text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-sm:text-center"
+      >
+        <div>
+          <div class="mb-3 font-medium">05+</div>
+          <div class="text-sm sm:text-base md:text-lg lg:text-xl">
+            Years of Experience
+          </div>
+        </div>
+        <div>
+          <div class="mb-3 font-medium">25+</div>
+          <div class="text-sm sm:text-base md:text-lg lg:text-xl">
+            Satisfied Clients
+          </div>
+        </div>
+        <div>
+          <div class="mb-3 font-medium">40+</div>
+          <div class="text-sm sm:text-base md:text-lg lg:text-xl">
+            Completed Projects
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
 .texture {
-    background-image: url('@/assets/images/Texture.png');
+  background-image: url("@/assets/images/Texture.png");
 }
 </style>
--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,59 +1,63 @@
 <script setup>
 import { Splide, SplideSlide } from "@splidejs/vue-splide";
 import "@splidejs/vue-splide/css";
+import { testimonials } from "@/helpers/testemonials";
 import Quote from "@/components/icons/Quote.vue";
 
 const options = {
   type: "loop",
   perPage: 1,
   focus: 0,
-  pagination: false
+  pagination: false,
 };
-
-const testimonials = [
-  {
-    name: "Hicham Abazine",
-    title: "Abdallah was a game changer",
-    role: "Backend & Mobile Developer",
-    Image: "../src/assets/images/avatars/HichamAbazine.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/hichamabazine/",
-      github: "https://github.com/Abdallah01win",
-    },
-    text: "For so long, I have struggled with establishing a strong online presence. Abdallah provided expertise and insights that I, even with my technical background, found invaluable. He was able to understand and easily communicate my values and vision in a way that resonated with my target audience. It's always a pleasure to work with him.",
-  },
-];
 </script>
 
 <template>
-  <section id="testimonials"
-    class="3xs:max-w-[320px] 2xs:max-w-[360px] xs:max-w-[390px] sm:max-w-[475px] md:max-w-[640px] lg:max-w-[768px] xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto pt-14 sm:pt-18 md:pt-24">
+  <section
+    id="testimonials"
+    class="3xs:max-w-[320px] 2xs:max-w-[360px] xs:max-w-[390px] sm:max-w-[475px] md:max-w-[640px] lg:max-w-[768px] xl:max-w-[1024px] 2xl:max-w-[1280px] mx-auto pt-14 sm:pt-18 md:pt-24"
+  >
     <div class="mb-10">
       <div
-        class="border border-myDark-100 py-2 px-6 w-fit h-fit rounded-full text-base lg:text-lg font-semibold mb-6 sm:mb-8 lg:mb-10 capitalize">
+        class="border border-myDark-100 py-2 px-6 w-fit h-fit rounded-full text-base lg:text-lg font-semibold mb-6 sm:mb-8 lg:mb-10 capitalize"
+      >
         testimonials
       </div>
 
       <p
-        class="lg:w-[90%] font-primary text-xl xs:text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-bold leading-[1.35] md:leading-[1.1] tracking-wide mb-10">
-        Hear it from countless satisfied clients who achieved remarkable results.
+        class="lg:w-[90%] font-primary text-xl xs:text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-bold leading-[1.35] md:leading-[1.1] tracking-wide mb-10"
+      >
+        Hear it from countless satisfied clients who achieved remarkable
+        results.
       </p>
     </div>
     <div class="border border-myGray-500/40 rounded-3xl">
       <Splide :options="options" aria-label="testimonials">
         <SplideSlide v-for="(item, index) in testimonials" :key="index">
-          <div class="flex flex-col items-center px-8 md:px-12 py-8 md:py-10 lg:py-16">
+          <div
+            class="flex flex-col items-center px-8 md:px-12 py-8 md:py-10 lg:py-16"
+          >
             <div>
-              <Quote class="w-6 md:w-8 h-6 md:h-8 text-myGray-500/40 mb-4 md:mb-9" />
+              <Quote
+                class="w-6 md:w-8 h-6 md:h-8 text-myGray-500/40 mb-4 md:mb-9"
+              />
             </div>
-            <div class="sm:text-xl md:text-3xl font-primaryBold mb-3 md:mb-5 lg:mb-3">"{{ item.title }}"</div>
+            <div
+              class="sm:text-xl md:text-3xl font-primaryBold mb-3 md:mb-5 lg:mb-3"
+            >
+              "{{ item.title }}"
+            </div>
             <p
-              class="leading-6 md:leading-8 text-sm md:text-base lg:text-lg font-medium text-center lg:w-[80%] mx-auto mb-6">
+              class="leading-6 md:leading-8 text-sm md:text-base lg:text-lg font-medium text-center lg:w-[80%] mx-auto mb-6"
+            >
               "{{ item.text }}"
             </p>
             <div class="inline-flex items-center">
-              <img alt="testimonial" :src="item.Image"
-                class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
+              <img
+                alt="testimonial"
+                :src="item.Image"
+                class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+              />
               <span class="flex-grow flex flex-col pl-4 gap-y-0.5">
                 <span class="font-semibold">{{ item.name }}</span>
                 <span class="text-sm text-myGray-400">{{ item.role }}</span>
